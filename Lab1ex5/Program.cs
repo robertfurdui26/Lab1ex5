c9b25af8ef43ac6ade1f59f92c810d0f39b0a1c6
robertfurdui26 <125736128+robertfurdui26@users.noreply.github.com>
2023-06-27 13:44:35 +0300

    Add project files.

---
+++ Lab1ex5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lab1ex5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Ex 5
+            Se citesc doua numere de la tastatura, x,y. Scrieti un program care va afisa cele doua valori in ordine crescatoare.
+            • Exemplu: citim ,9,0 Afisam : 0 9*/
+
+            Console.WriteLine("Introduce x: ");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduceti y: ");
+            int y = int.Parse(Console.ReadLine());
+
+            if(x > y)
+            {
+                Console.WriteLine("Afisam : " + y + " si " + x );
+            }
+            else
+            {
+                Console.WriteLine("Afisam : " + x + " si " + y);
+
+            }
+        }
+    }
+}
